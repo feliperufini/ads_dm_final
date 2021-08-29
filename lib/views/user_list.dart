eca99cb0e54020bbaa6a71c6570d7f4b41c82160
Felipe Andrade <feliperufini01@gmail.com>
2021-08-29 17:22:27 -0400

    Segundo Commit

---
+++ lib/views/user_list.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:ads_dm_final/components/usrer_tile.dart';
+import 'package:ads_dm_final/models/user.dart';
+import 'package:ads_dm_final/provider/users.dart';
+import 'package:ads_dm_final/routes/app_routes.dart';
+import 'package:provider/provider.dart';
+
+class UserList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Users users = Provider.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Usuários'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                AppRoutes.USER_FORM,
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: users.count,
+        itemBuilder: (ctx, i) => UserTile(users.byIndex(i)),
+      ),
+    );
+  }
+}
